d7dfdb06913186fba466c6a50db496f2a6862a70
BigBomb <seecsea@users.noreply.github.com>
2022-06-10 14:35:40 +0800

    Update rancher_uninstall.sh
    update ingress-nginx deploy

--- scripts/rancher_uninstall.sh
+++ scripts/rancher_uninstall.sh
@@ -56,4 +56,4 @@ aws route53 delete-hosted-zone --id $ZONE_ID
 rm -f rancher-record-set.json
 
 # Delete resources for NGINX Ingress in your cluster:
-kubectl delete -f https://github.com/aws-quickstart/quickstart-eks-rancher/blob/main/functions/source/kubernetes/ingress-nginx/controller-v0.40.2/deploy/static/provider/aws/deploy.yaml
+kubectl delete -f https://raw.githubusercontent.com/seecsea/quickstart-eks-rancher/main/scripts/deploy.yaml
